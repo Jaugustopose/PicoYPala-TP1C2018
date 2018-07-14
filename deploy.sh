ebb4fb7a863a091350e9ccdcfb875b7808d70dcd
Lucas Cabello <lucas_cabello@hotmail.com>
2018-07-14 17:30:12 -0300

    se corrige deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,30 @@
-mkdir TP_SO_2018
-cd TP_SO_2018
 git clone https://github.com/sisoputnfrba/so-commons-library
 git clone https://github.com/sisoputnfrba/parsi
+git clone https://github.com/sisoputnfrba/Pruebas-ESI
 cd so-commons-library
 sudo make install
 cd ..
 cd parsi
 sudo make install
 cd ..
-sudo make all
-
+cd comunicacion/Debug
+make all
+cd ..
+cd ..
+cd coordinador/Debug
+make all
+cd ..
+cd ..
+cd planificador/Debug
+make all
+cd ..
+cd ..
+cd instancia/Debug
+make all
+cd ..
+cd ..
+cd esi/Debug
+make all
+cd ..
+cd ..
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/workspace/tp-2018-1c-Pico-y-Pala/comunicacion/Debug
